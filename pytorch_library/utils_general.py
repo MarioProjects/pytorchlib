2aefd77c91c9a07803d8a7ba89e57fc5d8c33c1a
MarioProjects <maparla@inf.upv.es>
2018-10-25 13:46:12 +0200

    Añadida funcion topk

--- pytorch_library/utils_general.py
+++ pytorch_library/utils_general.py
@@ -1,3 +1,7 @@
+import numpy as np
+import torch
+from torch import nn, optim
+import torch.nn.functional as F
 from slackclient import SlackClient
 
 def slack_message(message, channel):
@@ -6,3 +10,11 @@ def slack_message(message, channel):
     sc.api_call('chat.postMessage', channel=channel,
                 text=message, username='My Sweet Bot',
                 icon_emoji=':robot_face:')
+
+def topk_classes(input, k):
+    """
+    Devuelve las k clases mas votadas de cada entrada de mayor a menor probabilidad
+    Para uso mas exhaustivo ir a https://pytorch.org/docs/stable/torch.html#torch.topk
+    """
+    probs_values, class_indxs = torch.topk(input, k)
+    return class_indxs
