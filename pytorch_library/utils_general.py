685417828431bd9eec859dfbabfa7095e0088507
MarioProjects <maparla@inf.upv.es>
2018-10-25 14:15:20 +0200

    Añadido models average con votacion y suma priors

--- pytorch_library/utils_general.py
+++ pytorch_library/utils_general.py
@@ -18,3 +18,30 @@ def topk_classes(input, k):
     """
     probs_values, class_indxs = torch.topk(input, k)
     return class_indxs
+
+def models_average(outputs, scheme):
+    """
+    Dada una lista de salidas (outputs) promedia los resultados obtenidos
+    teniendo dos posibildiades: 
+        - voting: donde la clase de mayor probabilidad vota 1 y el resto 0
+        - sum: se suma la probabilidad de todas las clases para decidir
+    """
+    # Si no utilizamos el numpy() el tensor va por referencia y 
+    # modifica la entrada original
+    if scheme == "sum":
+        result = outputs[0].data.cpu().numpy()
+        for output in outputs[1:]:
+            result += output.data.cpu().numpy()
+        return torch.from_numpy(result)
+    elif scheme == "voting":
+        # Es necesario tratar los outputs para pasarlos a codificacion 1 y 0s
+        # https://discuss.pytorch.org/t/keep-the-max-value-of-the-array-and-0-the-others/14480/7
+        view = outputs[0].view(-1, 5)
+        result = (view == view.max(dim=1, keepdim=True)[0]).view_as(outputs[0])
+        for output in outputs[1:]:
+            view = output.view(-1, 5)
+            result += (view == view.max(dim=1, keepdim=True)[0]).view_as(output)
+        return result
+    else: assert False, "Ivalid model average scheme!"
+
+
