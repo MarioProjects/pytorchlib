822281df8ba0b49e70c20e146d028a9abc10ea87
MarioProjects <maparla@inf.upv.es>
2018-10-30 10:28:25 +0100

    Añadidas funciones GAN

--- pytorch_library/utils_general.py
+++ pytorch_library/utils_general.py
@@ -3,6 +3,7 @@ import torch
 from torch import nn, optim
 import torch.nn.functional as F
 from slackclient import SlackClient
+from torch.autograd.variable import Variable
 
 def slack_message(message, channel):
     token = 'xoxp-458177397862-456198240913-464006277364-a7557a18c11f5e99ca9ce33deacbefc4'
@@ -11,5 +12,26 @@ def slack_message(message, channel):
                 text=message, username='My Sweet Bot',
                 icon_emoji=':robot_face:')
 
+def images_to_vectors(images):
+    """
+    Con esta funcion pasamos las imagenes a vectores -> Flatten
+    """
+    return images.view(images.size(0), -1).type(torch.cuda.FloatTensor)
+
+
+def vectors_to_images(vectors, width, height, depth):
+    """
+    Con esta funcion pasamos los vectores a imagenes
+    """
+    return vectors.view(vectors.size(0), depth, width, height).type(torch.cuda.FloatTensor)
+
+def noise(samples, out_features):
+    """
+    Con esta funcion creamos tantas muestras como 'samples'
+    formadas por ruido gausiano con una distribucion normal
+    con media 0 y varianza 1. Dimensionalidad out_features 
+    (la misma que toma el generador para samplear muestras)
+    """
+    return Variable(torch.randn(samples, out_features)).type(torch.cuda.FloatTensor)
 
 
